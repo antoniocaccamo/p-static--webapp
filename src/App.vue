fe9a4507b01b79cfe712a1414ab81c8200fa513a
antoni caccamo <caccamo.antonio@gmail.com>
2022-03-18 11:06:05 +0100

    UPD : simple api call

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
     };
   },
   async mounted() {
-    const { text } = await (await fetch("/api/message")).json();
+    const { text } = await (await fetch("/api/p-static-api-func-01")).json();
     this.message = text;
   }
 };
